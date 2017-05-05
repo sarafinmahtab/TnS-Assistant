4177e3bf695e7648f3f33b7cb2c492722dee01a5
Shamsul Arafin Mahtab <arafinmahtab@gmail.com>
2017-05-06 02:46:36 +0600

    Working in MySQL Database

--- app/src/main/java/com/sarafinmahtab/tnsassistant/LoginActivity.java
+++ app/src/main/java/com/sarafinmahtab/tnsassistant/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,21 +11,41 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import com.sarafinmahtab.tnsassistant.BackgroundWork.BackgroundWorker;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText email, password;
+
     private static RadioGroup radioGroup;
     private static RadioButton radioButton;
+    private static Button signIn, register;
+
+    public String server_url = "http://192.168.0.63/tnsAssistant/reg_response.php";
+
     public static int radio_key;
 
-    private EditText email, password;
-    private static Button signIn, register;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024*1024);
+        Network network = new BasicNetwork(new HurlStack());
+        requestQueue = new RequestQueue(cache, network);
+        requestQueue.start();
 
         radioClickAction();
         onLoginButtonClick();
@@ -67,8 +86,6 @@ public class LoginActivity extends AppCompatActivity {
                             radio_str = "Teacher";
                             String type = "Login";
                             Toast.makeText(LoginActivity.this, radio_str+'\n'+mail+'\n'+pass, Toast.LENGTH_SHORT).show();
-                            BackgroundWorker backgroundWorker = new BackgroundWorker(LoginActivity.this);
-                            backgroundWorker.execute(type, mail, pass);
                         } else if(radio_key == 2) {
                             radio_str = "Student";
                             Toast.makeText(LoginActivity.this, radio_str+'\n'+mail+'\n'+pass, Toast.LENGTH_SHORT).show();
@@ -89,9 +106,41 @@ public class LoginActivity extends AppCompatActivity {
                         Intent intent;
                         if(radio_key == 1) {
                             intent = new Intent("com.sarafinmahtab.tnsassistant.teacher.RegisterTeacher");
+                            StringRequest stringRequestforReg = new StringRequest(Request.Method.POST, server_url, new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Toast.makeText(LoginActivity.this, response, Toast.LENGTH_LONG).show();
+                                    requestQueue.stop();
+                                }
+                            }, new Response.ErrorListener(){
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast.makeText(LoginActivity.this, "An Error Occurred", Toast.LENGTH_LONG).show();
+                                    error.printStackTrace();
+                                    requestQueue.stop();
+                                }
+                            });
+
+                            requestQueue.add(stringRequestforReg);
                             startActivity(intent);
                         } else if(radio_key == 2) {
                             intent = new Intent("com.sarafinmahtab.tnsassistant.student.RegisterStudent");
+                            StringRequest stringRequestforReg = new StringRequest(Request.Method.POST, server_url, new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Toast.makeText(LoginActivity.this, response, Toast.LENGTH_LONG).show();
+                                    requestQueue.stop();
+                                }
+                            }, new Response.ErrorListener(){
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast.makeText(LoginActivity.this, "An Error Occurred", Toast.LENGTH_LONG).show();
+                                    error.printStackTrace();
+                                    requestQueue.stop();
+                                }
+                            });
+
+                            requestQueue.add(stringRequestforReg);
                             startActivity(intent);
                         } else {
                             Toast.makeText(LoginActivity.this, "You haven't checked any profile yet!", Toast.LENGTH_LONG).show();
