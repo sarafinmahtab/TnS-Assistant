20c367093b71dee20acfabafb4cde1ccc2d25e38
sarafinmahtab <arafinmahtab@gmail.com>
2017-06-15 05:51:42 +0600

    Some problem solved

---
+++ app/src/main/java/com/sarafinmahtab/tnsassistant/student/StudentActivity.java
@@ -0,0 +1,15 @@
+package com.sarafinmahtab.tnsassistant.student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sarafinmahtab.tnsassistant.R;
+
+public class StudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+    }
+}
--- app/src/main/java/com/sarafinmahtab/tnsassistant/teacher/Course.java
+++ app/src/main/java/com/sarafinmahtab/tnsassistant/teacher/Course.java
@@ -20,10 +20,6 @@ public class Course {
         return course_id;
     }
 
-    public void setCourse_id(String course_id) {
-        this.course_id = course_id;
-    }
-
     public String getCourse_code() {
         return course_code;
     }
