852920a6b5640d12478e4b1127773a71d3463ee6
sarafinmahtab <arafinmahtab@gmail.com>
2017-08-29 02:59:47 +0600

    Update Teacher MarkSheet Layout

--- app/src/main/java/com/sarafinmahtab/tnsassistant/teacher/marksheet/MarkUpdateAdapter.java
+++ app/src/main/java/com/sarafinmahtab/tnsassistant/teacher/marksheet/MarkUpdateAdapter.java
@@ -1,12 +1,15 @@
 package com.sarafinmahtab.tnsassistant.teacher.marksheet;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -51,12 +54,36 @@ public class MarkUpdateAdapter extends RecyclerView.Adapter<MarkUpdateAdapter.Ma
         final MarkListItem markListItem = newMarkListItem.get(position);
 
         holder.candidateReg.setText(String.format("Reg ID - %s", markListItem.getRegNo()));
-        holder.termTest1.setText(examType.getTest1() + ": " + markListItem.getTermTest1_Mark());
-        holder.termTest2.setText(examType.getTest2() + ": " + markListItem.getTermTest2_Mark());
-        holder.attendance.setText(examType.getAttendance() + ": " + markListItem.getAttendanceMark());
-        holder.viva.setText(examType.getViva() + ": " + markListItem.getVivaMark());
-        holder.finalExam.setText(examType.getFinal_exam() + ": " + markListItem.getFinalExamMark());
-        holder.marksOutOf100.setText(String.format("Marks in Total: %s", markListItem.getMarksOutOf100()));
+
+        holder.termTest1.setText(examType.getTest1());
+        holder.termTest1Mark.setText(markListItem.getTermTest1_Mark());
+
+        holder.termTest2.setText(examType.getTest2());
+        holder.termTest1Mark.setText(markListItem.getTermTest2_Mark());
+
+        holder.attendance.setText(examType.getAttendance());
+        holder.attendanceMark.setText(markListItem.getAttendanceMark());
+
+        holder.viva.setText(examType.getViva());
+        holder.vivaMark.setText(markListItem.getVivaMark());
+
+        holder.finalExam.setText(examType.getFinal_exam());
+        holder.finalExamMark.setText(markListItem.getFinalExamMark());
+
+        //Can Do the Calculation
+        holder.marksOutOf100Mark.setText(markListItem.getMarksOutOf100());
+        int[] randomColors = context.getResources().getIntArray(R.array.random_colors);
+
+        //Selects random colors from color Array
+//        int randomSelectedColor = randomColors[new Random().nextInt(randomColors.length)];
+
+        //Maintains the sorted colors from color Array
+        int randomSelectedColor = randomColors[position];
+
+        GradientDrawable bgShape = (GradientDrawable)holder.circleFinalMarks.getBackground();
+        bgShape.mutate();
+        bgShape.setColor(randomSelectedColor);
+
 
         holder.markUpdateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,7 +92,7 @@ public class MarkUpdateAdapter extends RecyclerView.Adapter<MarkUpdateAdapter.Ma
             }
         });
 
-        holder.markListConstraintLayout.setOnClickListener(new View.OnClickListener() {
+        holder.markListRelativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, markListItem.getCourseRegID(), Toast.LENGTH_LONG).show();
@@ -80,25 +107,36 @@ public class MarkUpdateAdapter extends RecyclerView.Adapter<MarkUpdateAdapter.Ma
 
     public static class MarkUpdateViewHolder extends RecyclerView.ViewHolder{
 
-        TextView candidateReg, termTest1, termTest2, attendance, viva, finalExam, marksOutOf100;
+        TextView candidateReg;
+        TextView termTest1, termTest2, attendance, viva, finalExam;
+        TextView termTest1Mark, termTest2Mark, attendanceMark, vivaMark, finalExamMark, marksOutOf100Mark;
+
         ImageButton markUpdateBtn;
 
-        ConstraintLayout markListConstraintLayout;
+        RelativeLayout markListRelativeLayout, circleFinalMarks;
 
         public MarkUpdateViewHolder(View itemView) {
             super(itemView);
 
             candidateReg = (TextView) itemView.findViewById(R.id.candidate_reg);
+
             termTest1 = (TextView) itemView.findViewById(R.id.term_test1);
             termTest2 = (TextView) itemView.findViewById(R.id.term_test2);
             attendance = (TextView) itemView.findViewById(R.id.attendance);
             viva = (TextView) itemView.findViewById(R.id.viva);
             finalExam = (TextView) itemView.findViewById(R.id.final_exam);
-            marksOutOf100 = (TextView) itemView.findViewById(R.id.marks_out_100);
 
+            termTest1Mark = (TextView) itemView.findViewById(R.id.term_test1_marks);
+            termTest2Mark = (TextView) itemView.findViewById(R.id.term_test2_marks);
+            attendanceMark = (TextView) itemView.findViewById(R.id.attendance_marks);
+            vivaMark = (TextView) itemView.findViewById(R.id.viva_marks);
+            finalExamMark = (TextView) itemView.findViewById(R.id.final_exam_marks);
+
+            circleFinalMarks = (RelativeLayout) itemView.findViewById(R.id.circle_final_marks);
+            marksOutOf100Mark = (TextView) itemView.findViewById(R.id.final_marks_oval_text);
+
+            markListRelativeLayout = (RelativeLayout) itemView.findViewById(R.id.mark_update_linear);
             markUpdateBtn = (ImageButton) itemView.findViewById(R.id.mark_update_btn);
-
-            markListConstraintLayout = (ConstraintLayout) itemView.findViewById(R.id.mark_update_linear);
         }
     }
 
